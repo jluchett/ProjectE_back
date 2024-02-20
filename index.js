64b8e49cfb00773214d36446ac2f71c355724ba7
jluchett <jluchett@hotmail.com>
2024-02-20 16:23:24 -0500

    configurado server Express y db.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hola mundo");
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+// Configuración del puerto
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+// Rutas
+app.get('/', (req, res) => {
+    res.send('¡Servidor de backend en funcionamiento cvzxvvc!');
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
