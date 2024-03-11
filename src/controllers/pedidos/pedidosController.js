361d6f62344beca395ae522671bf07999f75508e
jluchett <jluchett@hotmail.com>
2024-03-10 22:51:43 -0500

    agregado ruta y controler de listarPedidos

--- src/controllers/pedidos/pedidosController.js
+++ src/controllers/pedidos/pedidosController.js
@@ -1,5 +1,7 @@
 const registrarPedido = require('../pedidos/registrarPedido');
+const listarPedidos = require('../pedidos/listarPedidos');
 
 module.exports = {
     registrarPedido,
+    listarPedidos,
 }
--- src/routes/pedidos.js
+++ src/routes/pedidos.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const pedidosController = require('../controllers/pedidos/pedidosController');
 const auth = require('../middleware/auth');
+
 // Ruta para registrar un pedido
 router.post('/crear', pedidosController.registrarPedido);
-
+// Ruta para consultar todos los pedidos
+router.post('/listar', pedidosController.listarPedidos);
 
 module.exports = router;
