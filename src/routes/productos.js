1f3f32c5d30df72373f05063111c896c52ade701
jluchett <jluchett@hotmail.com>
2024-03-16 11:28:35 -0500

    configurado ruta para eliminar producto

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -3,13 +3,16 @@ const express = require('express');
 const router = express.Router();
 const productosController = require('../controllers/productos/productosController');
 const auth = require('../middleware/auth');
+
 // Ruta para registrar un producto
 router.post('/crear', productosController.crearProducto);
 // Ruta para actualizar un producto
 router.put('/update/:id', auth, productosController.actualizarProducto);
-// Ruta protegida para obtener listado de productos
+// Ruta para obtener listado de productos
 router.get('/listar', productosController.listarProductos);
 // Ruta para obtener un producto por su ID
 router.get('/get/:id', productosController.obtenerProducto);
+// Ruta para eliminar un producto por su ID
+router.delete('/del/:id', productosController.eliminarProducto);
 
 module.exports = router;
