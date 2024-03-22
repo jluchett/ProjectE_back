11703afdffb244e8e14b9aacbf93b595c5018132
jluchett <jluchett@hotmail.com>
2024-03-22 17:05:57 -0500

    Establecido controller y ruta de actualizar categoria

--- src/controllers/categorias/categoriasController.js
+++ src/controllers/categorias/categoriasController.js
@@ -1,7 +1,9 @@
 const crearCategoria = require("./crearCategoria");
 const listarCategorias = require("./listarCategorias");
+const updateCategoria = require('./actualizarCategoria')
 
 module.exports = {
   crearCategoria,
   listarCategorias,
+  updateCategoria,
 };
--- src/routes/categorias.js
+++ src/routes/categorias.js
@@ -5,5 +5,6 @@ const auth = require("../middleware/auth");
 
 router.post("/crear", categoriasController.crearCategoria);
 router.get('/listar', categoriasController.listarCategorias);
+router.put('/update', categoriasController.updateCategoria);
 
 module.exports = router;
